bf9313889ec3507041eb79d2fc19f03293a0662e
Tamer-cs <tameralafeef1@gmail.com>
2026-02-04 20:57:07 +0200

    use map for content parts

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -2,9 +2,13 @@ import Part from './Part.jsx'
 
 const Content = (props) => (
   <div>
-    <Part part={props.parts[0]} />
+    {/* <Part part={props.parts[0]} />
     <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+    <Part part={props.parts[2]} /> */}
+
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
   </div>
 )
 
