bfb030ac0fbdf38c99917c1aa5d977b13b7d36e7
Tamer-cs <tameralafeef1@gmail.com>
2026-02-04 20:49:49 +0200

    exercises 2.1 and 2.2

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,87 @@
+// const Header = (props) => <h1>{props.course}</h1>
+
+// const Content = (props) => (
+//   <div>
+//     <Part part={props.parts[0]} />
+//     <Part part={props.parts[1]} />
+//     <Part part={props.parts[2]} />
+//   </div>
+// )
+
+// const Part = (props) => (
+//   <p>
+//     {props.part.name} {props.part.exercises}
+//   </p>
+// )
+
+// const Total = (props) => <p>Number of exercises {props.total}</p>
+
+// const App = () => {
+//   const course = {
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         exercises: 10,
+//       },
+//       {
+//         name: 'Using props to pass data',
+//         exercises: 7,
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14,
+//       },
+//     ],
+//   }
+
+//   return (
+//     <div>
+//       <Header course={course.name} />
+//       <Content parts={course.parts} />
+//       <Total
+//         total={
+//           course.parts[0].exercises +
+//           course.parts[1].exercises +
+//           course.parts[2].exercises
+//         }
+//       />
+//     </div>
+//   )
+// }
+
+// export default App
+
+import Course from './components/Course'
+import Header from './components/Header'
+import Content from './components/Content'
+import Part from './components/Part'
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
