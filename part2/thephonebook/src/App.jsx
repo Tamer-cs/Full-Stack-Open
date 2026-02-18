eb7d08d84825ef2c2fa7b8e9772a231c7655c1a0
Tamer-cs <tameralafeef1@gmail.com>
2026-02-18 11:03:58 +0200

    exercise 2.6

---
+++ part2/thephonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+
+  const handleInput = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+      event.preventDefault()
+      const personObject = {
+        name: newName
+      }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit = {handleSubmit}>
+        <div>
+          name: <input value = {newName} onChange = {handleInput}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+    </div>
+  )
+}
+
+export default App
