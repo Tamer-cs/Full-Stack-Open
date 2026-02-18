98e41e35e372f5c01b0cb4a091f571eeb894855f
Tamer-cs <tameralafeef1@gmail.com>
2026-02-18 17:38:03 +0200

    exercise 2.9

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' ,
-      phone_number: '00 000 000'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setNewFilter] = useState('')
+
 
   const handleName = (event) => {
     setNewName(event.target.value) 
@@ -16,20 +19,32 @@ const App = () => {
   const handleNumber = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleSearch = (event) => {
+    setNewFilter(event.target.value)
+  }
+  
 
   const handleSubmit = (event) => {
     event.preventDefault()
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to the phonebook`)
     } else {
-      const temp = { name: newName , phone_number: newNumber }
+      const temp = { name: newName , number: newNumber, id: persons.length + 1 }
       setPersons(persons.concat(temp))
     }
   }
 
+const persons_to_show = persons.filter(person =>
+  person.name.toLowerCase().includes(filter.toLowerCase())
+)
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value = {filter} onChange = {handleSearch}/>
+      </div>
+      <h2>Add a New</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleName} />
@@ -42,7 +57,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map( person => <div key = {person.name} >  {person.name} {person.phone_number} </div> )}
+      <div>
+      {persons_to_show.map( person => <div key = {person.id} >  {person.name} {person.number} </div> )}
+      </div>
+
     </div>
   )
 }
