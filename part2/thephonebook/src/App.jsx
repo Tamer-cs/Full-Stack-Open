792b4c510b2bebe293e5063e7c99e38bd05be3a6
Tamer-cs <tameralafeef1@gmail.com>
2026-03-11 19:24:31 +0200

    exercise 2.11

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,24 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './compnents/Filter'
 import PersonForm from './compnents/PersonForm'
 import Persons from './compnents/Persons'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  // ])
 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('')
 
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
+  }
+  useEffect(hook, [])
 
   const handleName = (event) => {
-    setNewName(event.target.value) 
+    setNewName(event.target.value)
   }
 
   const handleNumber = (event) => {
@@ -27,30 +35,30 @@ const App = () => {
 
   const handleSearch = (event) => {
     setNewFilter(event.target.value)
-  }  
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to the phonebook`)
     } else {
-      const temp = { name: newName , number: newNumber, id: persons.length + 1 }
+      const temp = { name: newName, number: newNumber, id: persons.length + 1 }
       setPersons(persons.concat(temp))
     }
   }
 
-const persons_to_show = persons.filter(person =>
-  person.name.toLowerCase().includes(filter.toLowerCase())
-)
+  const persons_to_show = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filter = {filter} handleSearch = {handleSearch} />
+      <Filter filter={filter} handleSearch={handleSearch} />
       <h2>Add a New</h2>
-      <PersonForm handleSubmit = {handleSubmit} newName = {newName} handleName = {handleName} newNumber = {newNumber} handleNumber = {handleNumber} />
+      <PersonForm handleSubmit={handleSubmit} newName={newName} handleName={handleName} newNumber={newNumber} handleNumber={handleNumber} />
       <h2>Numbers</h2>
-      <Persons persons_to_show = {persons_to_show} />
+      <Persons persons_to_show={persons_to_show} />
     </div>
   )
 }
