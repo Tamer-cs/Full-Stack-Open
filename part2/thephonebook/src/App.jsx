0584195199c0750e7898f48309a53c3bbb16d00d
Tamer-cs <tameralafeef1@gmail.com>
2026-03-20 14:22:55 +0200

    exercise 2.13

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -3,15 +3,9 @@ import axios from 'axios'
 import Filter from './compnents/Filter'
 import PersonForm from './compnents/PersonForm'
 import Persons from './compnents/Persons'
+import personService from './services/personsService'
 
 const App = () => {
-
-  // const [persons, setPersons] = useState([
-  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  // ])
 
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
@@ -19,8 +13,8 @@ const App = () => {
   const [filter, setNewFilter] = useState('')
 
   const hook = () => {
-    axios.get('http://localhost:3001/persons').then(response => {
-      setPersons(response.data)
+    personService.getAll().then(data => {
+      setPersons(data)
     })
   }
   useEffect(hook, [])
@@ -43,7 +37,9 @@ const App = () => {
       alert(`${newName} is already added to the phonebook`)
     } else {
       const temp = { name: newName, number: newNumber, id: persons.length + 1 }
-      setPersons(persons.concat(temp))
+      personService.create(temp).then(data => {
+        setPersons(persons.concat(data))
+      })
     }
   }
 
